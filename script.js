c5be6ddcf8631bf3a613d5733d0ffa4a3305973f
Terrence <netceer112@gmail.com>
2020-06-25 21:38:55 +0100

    feat: add output display and decimal limitation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  const deleteButton = document.querySelector("#delete");
 
  const numberButtons = [...document.querySelectorAll(".number-button")];
-const allButtons = [...document.querySelectorAll("button")];
+ const allButtons = [...document.querySelectorAll("button")];
 
 function clearFunction() {
     outputDisplay.style.visibility = "hidden";
@@ -15,22 +15,31 @@ function clearFunction() {
 clearButton.addEventListener("click", clearFunction);
 
 function deleteFunction() {
-    inputDisplay.textContent = inputDisplay.textContent.slice(0,-1);
-    if(inputDisplay.textContent.length === 0) inputDisplay.textContent = "0";
+    inputDisplay.innerText = inputDisplay.innerText.slice(0,-1);
+    if(inputDisplay.innerText.length === 0) {
+        inputDisplay.innerText = "0";
+        outputDisplay.innerText = "";
+        outputDisplay.style.visibility = "hidden";
+    }
+    outputDisplay.innerText = inputDisplay.innerText;
 }
 
 deleteButton.addEventListener("click", deleteFunction);
 
 
 function addButtonToInputDisplay(button) {
-    if (/AC|DEL/g.test(button.innerText)) return;
+    if(/AC|DEL/g.test(button.innerText)) return;
+    if(inputDisplay.innerText == "0") inputDisplay.innerText = "";
     
+    if(button.innerText === "." && inputDisplay.innerText.includes(".")) return;
+
     inputDisplay.innerText += button.innerText;
+    outputDisplay.style.visibility = "visible";
+    outputDisplay.innerText = inputDisplay.innerText;
 };
 
 allButtons.map(button => {
     button.addEventListener("click", () => addButtonToInputDisplay(button) )
     }
 );
-
 
