cc1e4039f3074f63ba3f523df5ecf4ca2f50dafc
Terrence <netceer112@gmail.com>
2020-06-28 20:48:00 +0100

    feat: add ongoing calculation to output display

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
- const clearButton = document.querySelector("#clear");
- const outputDisplay = document.querySelector("#output-display");
- const inputDisplay = document.querySelector("#input-display");
- const deleteButton = document.querySelector("#delete");
- const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const outputDisplay = document.querySelector("#output-display");
+const inputDisplay = document.querySelector("#input-display");
+const deleteButton = document.querySelector("#delete");
+const equalsButton = document.querySelector("#equals");
 
-
- const numberButtons = [...document.querySelectorAll(".number-button")];
- const allButtons = [...document.querySelectorAll("button")];
- const operatorButtons = [...document.querySelectorAll(".operator-button")];
+const numberButtons = [...document.querySelectorAll(".number-button")];
+const allButtons = [...document.querySelectorAll("button")];
+const operatorButtons = [...document.querySelectorAll(".operator-button")];
 
 function clearFunction() {
     outputDisplay.style.visibility = "hidden";
@@ -18,97 +17,103 @@ function clearFunction() {
 clearButton.addEventListener("click", clearFunction);
 
 function deleteFunction() {
-    inputDisplay.innerText = inputDisplay.innerText.slice(0,-1);
-    if(inputDisplay.innerText.length === 0) {
+    inputDisplay.innerText = inputDisplay.innerText.slice(0, -1);
+
+    if (inputDisplay.innerText.length === 0) {
         inputDisplay.innerText = "0";
         outputDisplay.innerText = "";
         outputDisplay.style.visibility = "hidden";
     }
+
     outputDisplay.innerText = inputDisplay.innerText;
+    outputDisplay.innerText = calculate();
 }
 
 deleteButton.addEventListener("click", deleteFunction);
 
 
 function addButtonToInputDisplay(button) {
-    if(/AC|DEL/g.test(button.innerText)) return;
-    if(inputDisplay.innerText == "0" && button.innerText !== ".") inputDisplay.innerText = "";
-    if(button.innerText === "." && inputDisplay.innerText.includes(".")) return;
-    if(/[+÷×−=]/g.test(button.innerText)) return;
-    if(/[+÷×=]/g.test(inputDisplay.innerText.slice(0,1))) inputDisplay.innerText = "";
+    if (/AC|DEL/g.test(button.innerText)) return;
+    if (inputDisplay.innerText == "0" && button.innerText !== ".") inputDisplay.innerText = "";
+    if (outputDisplay.innerText == "0" && button.innerText !== ".") outputDisplay.innerText = "";
+    if (button.innerText === "." && inputDisplay.innerText.includes(".")) return;
+    if (/[+÷×−=]/g.test(button.innerText)) return;
+    if (/[+÷×=]/g.test(inputDisplay.innerText.slice(0, 1))) inputDisplay.innerText = "";
 
     inputDisplay.innerText += button.innerText;
     outputDisplay.style.visibility = "visible";
-    outputDisplay.innerText = inputDisplay.innerText;
+    outputDisplay.innerText += button.innerText;
+    outputDisplay.innerText = calculate();
 };
 
 allButtons.map(button => {
-    button.addEventListener("click", () => addButtonToInputDisplay(button) )
-    }
+    button.addEventListener("click", () => addButtonToInputDisplay(button))
+}
 );
 
 function handleOperatorInput(operator) {
 
-    if(/[+÷×−]/g.test(inputDisplay.innerText.slice(-1)) && /[+÷×−]/g.test(inputDisplay.innerText.slice(-2,-1)) && /[+÷×−]/g.test(operator.innerText)) {
+    if (/[+÷×−]/g.test(inputDisplay.innerText.slice(-1)) && /[+÷×−]/g.test(inputDisplay.innerText.slice(-2, -1)) && /[+÷×−]/g.test(operator.innerText)) {
 
-        if(inputDisplay.innerText.slice(-1) === "−" && operator.innerText === "−") {
-            inputDisplay.innerText = inputDisplay.innerText.slice(0,-2) + "+";
-            outputDisplay.style.visibility = "visible";
-            outputDisplay.innerText = inputDisplay.innerText;
-            return;}
+        if (inputDisplay.innerText.slice(-1) === "−" && operator.innerText === "−") {
+            inputDisplay.innerText = inputDisplay.innerText.slice(0, -2) + "+";;
+            outputDisplay.innerText = outputDisplay.innerText.slice(0, -2) + "+";
+            return;
+        }
 
-       inputDisplay.innerText = inputDisplay.innerText.slice(0,-2) + operator.innerText;
-       outputDisplay.style.visibility = "visible";
-       outputDisplay.innerText = inputDisplay.innerText;
-       return;
+        inputDisplay.innerText = inputDisplay.innerText.slice(0, -2) + operator.innerText;
+        outputDisplay.innerText = outputDisplay.innerText.slice(0, -2) + operator.innerText;
+        return;
     }
 
-    if(/[+÷×−]/g.test(inputDisplay.innerText.slice(-1))) {
+    if (/[+÷×−]/g.test(inputDisplay.innerText.slice(-1))) {
 
-        if(inputDisplay.innerText.slice(-1) === "−" && operator.innerText === "−") {
-            inputDisplay.innerText = inputDisplay.innerText.slice(0,-1) + "+";
-            if(/[+÷×−]/g.test(inputDisplay.innerText.slice(-2,-1))) {
-                inputDisplay.innerText = inputDisplay.innerText.slice(0,-1);
+        if (inputDisplay.innerText.slice(-1) === "−" && operator.innerText === "−") {
+            inputDisplay.innerText = inputDisplay.innerText.slice(0, -1) + "+";
+            outputDisplay.innerText = outputDisplay.innerText.slice(0, -1) + "+";
+            if (/[+÷×−]/g.test(inputDisplay.innerText.slice(-2, -1))) {
+                inputDisplay.innerText = inputDisplay.innerText.slice(0, -1);
+                outputDisplay.innerText = outputDisplay.innerText.slice(0, -1);
             }
-            outputDisplay.style.visibility = "visible";
-            outputDisplay.innerText = inputDisplay.innerText;
-            return;}
+            return;
+        }
 
-            if(inputDisplay.innerText.slice(-1) === "+" && operator.innerText === "−") {
-                inputDisplay.innerText = inputDisplay.innerText.slice(0,-1) + "−";
-                outputDisplay.style.visibility = "visible";
-                outputDisplay.innerText = inputDisplay.innerText;
-                return;}
+        if (inputDisplay.innerText.slice(-1) === "+" && operator.innerText === "−") {
+            inputDisplay.innerText = inputDisplay.innerText.slice(0, -1) + "−";
+            outputDisplay.innerText = outputDisplay.innerText.slice(0, -1) + "−";
+            return;
+        }
 
-        if(operator.innerText === "−" && /[+÷×−]/g.test(inputDisplay.innerText.slice(-1))) { 
+        if (operator.innerText === "−" && /[+÷×−]/g.test(inputDisplay.innerText.slice(-1))) {
             inputDisplay.innerText += operator.innerText
-        return;}
+            outputDisplay.innerText += operator.innerText
+            return;
+        }
 
-       inputDisplay.innerText = inputDisplay.innerText.slice(0,-1) + operator.innerText;
-       outputDisplay.style.visibility = "visible";
-       outputDisplay.innerText = inputDisplay.innerText;
-       return;
+        inputDisplay.innerText = inputDisplay.innerText.slice(0, -1) + operator.innerText;
+        outputDisplay.innerText = outputDisplay.innerText.slice(0, -1) + operator.innerText;
+        return;
     }
     inputDisplay.innerText += operator.innerText;
     outputDisplay.style.visibility = "visible";
-    outputDisplay.innerText = inputDisplay.innerText;
+    outputDisplay.innerText += operator.innerText;
 };
 
 operatorButtons.map(operator => {
     operator.addEventListener("click", () => handleOperatorInput(operator))
 });
 
-function convertSymbols(inputString){
+function convertSymbols(inputString) {
     return inputString.replace(/×/g, "*")
-                      .replace(/÷/g, "/")
-                      .replace(/−/g, "-");
+        .replace(/÷/g, "/")
+        .replace(/−/g, "-");
 }
 
 function calculate() {
     let inputString = inputDisplay.innerText;
-    if(inputString === "") inputString = "0";
+    if (inputString === "") inputString = "0";
     let convertedString = convertSymbols(inputString);
-    inputDisplay.innerText = eval(convertedString);
+    return eval(convertedString);
 }
 
-equalsButton.addEventListener("click", () => calculate())
+equalsButton.addEventListener("click", () => inputDisplay.innerText = calculate())
